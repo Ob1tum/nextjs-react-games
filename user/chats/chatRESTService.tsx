e9841ee70abf282b1a73480b9983e9c9552d4de4
yushever <iushina.tatiana@gmail.com>
2023-03-03 16:08:14 +0500

    ws

--- user/chats/chatRESTService.tsx
+++ user/chats/chatRESTService.tsx
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
+let token =
+  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxQHEucSIsImlhdCI6MTY3NzgxNTYzNCwiZXhwIjoxNjc3OTAyMDM0fQ.omxxJlRa_K1blICsG1rePG9a8JQFmShPb44gUGbAfp5hJ303BQPuYTKyv34o7J28R6f2tLLmTpCrUTFQem7mEg';
+
 export default class ChatCreation {
   async createChat(userId?: number, token?: string) {
     let header = {
       headers: {
-        Authorization: `${'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyMTAwQG1haWwucnUiLCJpYXQiOjE2Nzc3NTc2NDcsImV4cCI6MTY3Nzg0NDA0N30.npv54EWA0Oef9zt2zHi3_YSkrcx98ffGX_BxscCCx1HEG0E6C0hRsyyN6pwoPowFvNjIqeud_9ULzjZD8dZIbw'}`,
+        Authorization: `Bearer ${token}`,
       },
     };
     let res = await axios.get(
       `http://91.241.64.78:8088/api/chats/single-chats/user/${userId}`,
       header,
     );
-    console.log(res);
-    return res.data;
+    return res;
   }
 }
--- user/chats/chatService.tsx
+++ user/chats/chatService.tsx
@@ -4,9 +4,8 @@ let token =
   'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxQHEucSIsImlhdCI6MTY3NzgxNTYzNCwiZXhwIjoxNjc3OTAyMDM0fQ.omxxJlRa_K1blICsG1rePG9a8JQFmShPb44gUGbAfp5hJ303BQPuYTKyv34o7J28R6f2tLLmTpCrUTFQem7mEg';
 
 export default class ChatService {
-  socket = io('ws://91.241.64.78:8088/ws', {
+  socket = io('http://91.241.64.78:8088/ws', {
     extraHeaders: {
-      'Access-Control-Allow-Origin': '*',
       Authorization: `Bearer ${token}`,
     },
   });
@@ -17,9 +16,9 @@ export default class ChatService {
       console.log('Connected', this.socket.id);
     });
 
-    this.socket.on('message', (data) => {
-      console.log(data);
-    });
+    // this.socket.on('message', (data) => {
+    //   console.log(data);
+    // });
 
     // socket.on('disconnect', () => {
     //   console.log('Disconnected', socket.id); // undefined
