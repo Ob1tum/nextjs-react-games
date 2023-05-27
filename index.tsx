c106a03bcf85871da9358455699893355dd96f35
german <germanbadin1338@gmail.com>
2023-05-27 19:38:28 +0500

    fix

--- index.tsx
+++ index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import { store } from './store/index';
 import { Main } from './user/main/main';
-// import './index.scss';
+// import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
