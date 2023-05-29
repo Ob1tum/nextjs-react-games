15f7133e249e670ea6c435b4ea8fe2116d5e3161
german <germanbadin1338@gmail.com>
2023-05-29 18:46:52 +0500

    fix 4

--- statisctics/components/TopProducts/TopProducts.tsx
+++ statisctics/components/TopProducts/TopProducts.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import classes from "./TopProducts.module.scss";
-const TopProducts = () => {
-  return (
-    <div className={classes.TopProducts}>
-      <h2>Top Games</h2>
-      <table>
-        <thead>
+
+import classes from './TopProducts.module.scss';
+
+const TopProducts = () => (
+  <div className={classes.TopProducts}>
+    <h2>Top Games</h2>
+    <table>
+      <thead>
         <tr>
           <th>#</th>
           <th>Name</th>
           <th>Popularity</th>
           <th>Sales</th>
         </tr>
-        </thead>
-        <tbody>
+      </thead>
+      <tbody>
         <tr>
           <td>1</td>
           <td>Chess</td>
           <td className={classes.popularity}>
-            <div className={classes.progress}
-                 style={{
-                   backgroundColor: '#cde7ff',
-                 }}
+            <div
+              className={classes.progress}
+              style={{
+                backgroundColor: '#cde7ff',
+              }}
             >
-              <div className={classes.progress__line} style={{
-                backgroundColor:'#0095ff',
-                width: `${50}%`
-              }}/>
+              <div
+                className={classes.progress__line}
+                style={{
+                  backgroundColor: '#0095ff',
+                  width: `${50}%`,
+                }}
+              />
             </div>
           </td>
           <td>{`${50}%`}</td>
@@ -35,15 +40,19 @@ const TopProducts = () => {
           <td>2</td>
           <td>Tanks</td>
           <td className={classes.popularity}>
-            <div className={classes.progress}
-                 style={{
-                   backgroundColor: '#8cfac7',
-                 }}
+            <div
+              className={classes.progress}
+              style={{
+                backgroundColor: '#8cfac7',
+              }}
             >
-              <div className={classes.progress__line} style={{
-                backgroundColor:'#00e096',
-                width: `${20}%`
-              }}/>
+              <div
+                className={classes.progress__line}
+                style={{
+                  backgroundColor: '#00e096',
+                  width: `${20}%`,
+                }}
+              />
             </div>
           </td>
           <td>{`${20}%`}</td>
@@ -52,15 +61,19 @@ const TopProducts = () => {
           <td>3</td>
           <td>Tetris</td>
           <td className={classes.popularity}>
-            <div className={classes.progress}
-                 style={{
-                   backgroundColor: '#c5a8ff',
-                 }}
+            <div
+              className={classes.progress}
+              style={{
+                backgroundColor: '#c5a8ff',
+              }}
             >
-              <div className={classes.progress__line} style={{
-                backgroundColor:'#884dff',
-                width: `${16}%`
-              }}/>
+              <div
+                className={classes.progress__line}
+                style={{
+                  backgroundColor: '#884dff',
+                  width: `${16}%`,
+                }}
+              />
             </div>
           </td>
           <td>{`${16}%`}</td>
@@ -69,23 +82,26 @@ const TopProducts = () => {
           <td>4</td>
           <td>Doodle Jump</td>
           <td className={classes.popularity}>
-            <div className={classes.progress}
-                 style={{
-                   backgroundColor: '#ffd5a4',
-                 }}
+            <div
+              className={classes.progress}
+              style={{
+                backgroundColor: '#ffd5a4',
+              }}
             >
-              <div className={classes.progress__line} style={{
-                backgroundColor:'#ff8f0d',
-                width: `${14}%`
-              }}/>
+              <div
+                className={classes.progress__line}
+                style={{
+                  backgroundColor: '#ff8f0d',
+                  width: `${14}%`,
+                }}
+              />
             </div>
           </td>
           <td>{`${14}%`}</td>
         </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
+      </tbody>
+    </table>
+  </div>
+);
 
 export default TopProducts;
