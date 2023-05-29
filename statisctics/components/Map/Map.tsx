b0aa493ad66b6357e3b0fd134e6c99002ac248e0
german <germanbadin1338@gmail.com>
2023-05-29 17:02:56 +0500

    fix 3

--- statisctics/components/Map/Map.tsx
+++ statisctics/components/Map/Map.tsx
@@ -2,29 +2,24 @@
 
 import React, { useEffect, useState } from 'react';
 import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet';
+import { GeoJsonObject } from 'geojson';
 
 import classes from './Map.module.scss';
+
 import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
-  const [geoJSON, setGeoJSON] = useState({});
-
+  const [geoJSON, setGeoJSON] = useState<GeoJsonObject>({ type: 'Point' });
+  const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
-    if (Object.keys(geoJSON).length === 0) {
+    if (!isLoaded) {
       fetch(
         'https://nominatim.openstreetmap.org/search.php?q=Russia&polygon_geojson=1&format=geojson',
       )
         .then((res) => res.json())
-        .then((json) => {
-          const data = {
-            type: '',
-            licence: '',
-            features: {},
-          };
-          data.type = json.type;
-          data.licence = json.licence;
-          data.features = json.features.filter((el) => el.geometry.type !== 'Point');
-          setGeoJSON(data);
+        .then((json: GeoJsonObject) => {
+          setGeoJSON(json);
+          setIsLoaded(true);
         });
     }
   });
@@ -36,7 +31,7 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {Object.keys(geoJSON).length !== 0 ? (
+        {isLoaded ? (
           <GeoJSON
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             data={geoJSON}
